3e4a4927982dbc155f61e44615e039a547823d31
Miri <little.ej3@gmail.com>
2023-08-08 19:49:37 +0300

    afterWebinar

--- src/main/java/Config/AppiumConfig.java
+++ src/main/java/Config/AppiumConfig.java
@@ -1,2 +1,48 @@
-package Config;public class AppiumConfig {
+package Config;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AppiumConfig {
+    public static AppiumDriver<MobileElement> driver; // static fo priority
+    // "appPackage": "com.telran.ilcarro",
+
+    @BeforeSuite
+    public void setUp() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Nex6");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"8.0");
+        capabilities.setCapability("appPackage", "com.telran.ilcarro");
+        capabilities.setCapability("appActivity", ".SplashActivity");
+
+
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        capabilities.setCapability(MobileCapabilityType.APP, "C:\\Tools\\apk\\ilcarro-android.apk");
+        /*
+        For Mac
+        capabilities.setCapability("app", "/Users/julia/Tools/ilcarro-android.apk");
+         */
+
+        try {
+            driver = new AppiumDriver<MobileElement>(new URL("http://localhost:4723/wd/hub"),capabilities);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @AfterSuite
+    public void tearDown(){
+           driver.quit();
+    }
+
+
 }
---
+++ src/test/java/Tests/SplashTests.java
@@ -0,0 +1,18 @@
+package Tests;
+
+import Config.AppiumConfig;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.SplashPage;
+
+public class SplashTests extends AppiumConfig {
+    String title2 = "The Ultimate Car Rental Service".toUpperCase();
+
+    @Test
+    public void title2Test(){
+        SplashPage splashPage = new SplashPage(driver);
+
+        Assert.assertEquals(splashPage.getTextTitle2(), title2);
+    }
+
+}
